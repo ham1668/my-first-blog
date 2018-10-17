24c77ca7a0bd98dc564f6236e534a7af46b147ad
ham1668 <ham1668@hotmail.com>
2018-10-17 09:25:52 -0700

    First commit for the Djangogirls app

---
+++ python_intro.py
@@ -0,0 +1,51 @@
+# print("Hello, John!")
+
+# if 3 > 2:
+# 	  print('It works!')
+
+# name = 'Sonja'
+# if name == 'Ola':
+#     print('Hey Ola!')
+# elif name == 'Sonja':
+#     print('Hey Sonja!')
+# else:
+#     print('Hey anonymous!')
+
+# volume = 57
+# if volume < 20:
+# 	print("It's kinda quiet.")
+# elif 20 <= volume < 40:
+# 	print("It's nice for background music")
+# elif 40 <= volume < 60:
+# 	print("Perfect, I can hear all the details")
+# elif 60 <= volume < 80:
+# 	print("Nice for parties")
+# elif 80 <= volume < 100:
+# 	print("A bit loud!")
+# else:
+# 	print("My ears are hurting! :(")
+
+# Functions!
+# def hi(name):
+# 	if name == 'Ola':
+# 		print('Hi Ola!')
+# 	elif name == 'Sonja':
+# 		print('Hi Sonja!')
+# 	else:
+# 		print('Hi anonymous!')
+
+# hi('John')
+
+# def hi(name):
+# 	print('Hi ' + name + '!')
+
+# hi('John')
+
+# girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+# for name in girls:
+# 	hi(name)
+# 	print('Next girl')
+
+for i in range(1, 6):
+	print(i)
